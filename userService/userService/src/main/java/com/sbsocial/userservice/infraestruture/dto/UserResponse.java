2116bda8e4e39d1974c97ed2ffb525797547d3ef
Saul Burgos <saulburgos6@gmail.com>
2023-08-21 18:11:50 -0400

    frontend init

---
+++ userService/userService/src/main/java/com/sbsocial/userservice/infraestruture/dto/UserResponse.java
@@ -0,0 +1,4 @@
+package com.sbsocial.userservice.infraestruture.dto;
+
+public record UserResponse() {
+}
---
+++ userService/userService/src/main/java/com/sbsocial/userservice/infraestruture/utils/ValidateErrors.java
@@ -0,0 +1,4 @@
+package com.sbsocial.userservice.infraestruture.utils;
+
+public class ValidateErrors {
+}
