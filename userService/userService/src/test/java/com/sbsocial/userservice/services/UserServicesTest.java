0a1930a4245386d9fd87cc8f24fb0267af203073
Saul Burgos <saulburgos6@gmail.com>
2023-09-19 15:31:20 -0400

    adding test to service users

---
+++ userService/userService/src/test/java/com/sbsocial/userservice/services/UserServicesTest.java
@@ -0,0 +1,24 @@
+package com.sbsocial.userservice.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServicesTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void findOne() {
+    }
+
+    @Test
+    void findOneByEmail() {
+    }
+
+    @Test
+    void findProfile() {
+    }
+}
