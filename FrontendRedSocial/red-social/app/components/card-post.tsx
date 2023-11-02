fa73fdd99d3b2040a404d35f32038b5ba4c7c9a1
osmait <saulburgos6@gmail.com>
2023-11-01 20:06:34 -0400

    remove local users local state add global users state and add skeleton loader

--- FrontendRedSocial/red-social/app/components/card-post.tsx
+++ FrontendRedSocial/red-social/app/components/card-post.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
   Card,
   CardHeader,
@@ -10,58 +10,60 @@ import {
 
 import { IconHeart } from "@tabler/icons-react";
 import { ModalComment } from "../../components/modal-comment";
-import { PostResponse, User } from "../../types";
-import { findProfile } from "../services/userService";
+import { PostResponse } from "../../types";
 import Link from "next/link";
+import { useNotification } from "../store/state";
+import LoadingSkeleton from "./LoadingSkeleton";
 
 export interface Props {
   post: PostResponse;
 }
 
 export default function CardPost({ post }: Props) {
-  const [user, setUser] = useState<User>();
-  useEffect(() => {
-    const getUser = async () => {
-      const user = await findProfile(post.post.userId);
-      setUser(user);
-    };
-    getUser();
-  }, []);
+  const user = useNotification(state => state.user)
+
+  const imgUrl = user?.img ? user.img : "https://avatars.dicebear.com/v2/male/dec006c73441fcd643d5cc092c35d14c.svg"
   return (
     <Card className="max-w-[750px] hover:bg-zinc-900 bg-black border-b-1 border-zinc-600 rounded-none ">
-      <CardHeader className="justify-between">
-        <div className="flex gap-5">
-          <Avatar
-            isBordered
-            radius="full"
-            size="md"
-            src={`https://unavatar.io/${user?.name}`}
-          />
-          <div className="flex gap-1 items-center justify-center">
-            <Link href={`/home/profile/${user?.id}`} className="hover:border-b-1">
-              <h4 className="text-small font-semibold leading-none text-default-600">
-                {`${user?.name} ${user?.LastName}`}
-              </h4>
+      {
+        user ?
+          <>
+            <CardHeader className="justify-between">
+              <div className="flex gap-5">
+                <Avatar
+                  isBordered
+                  radius="full"
+                  size="md"
+                  src={imgUrl}
+                />
+                <div className="flex gap-1 items-center justify-center">
+                  <Link href={`/home/profile/${user?.id}`} className="hover:border-b-1">
+                    <h4 className="text-small font-semibold leading-none text-default-600">
+                      {`${user?.name} ${user?.lastName}`}
+                    </h4>
+                  </Link>
+                  <h5 className="text-small tracking-tight text-default-400">
+                    {`@${user?.name}${user?.lastName}`}
+                  </h5>
+                </div>
+              </div>
+            </CardHeader>
+
+            <Link href={`/home/post/${post.post.id}`}>
+              <CardBody className="px-3 py-0 text-small  text-white">
+                <p>{post.post.content}</p>
+              </CardBody>
+              <CardFooter className="gap-3">
+                <button type="button">
+                  <IconHeart className="w-4 h-4" color="#71767B" />
+                </button>
+                <div className="flex gap-1  text-zinc-500">
+                  <ModalComment comments={post.comment} />
+                </div>
+              </CardFooter>
             </Link>
-            <h5 className="text-small tracking-tight text-default-400">
-              {`@${user?.name}${user?.LastName}`}
-            </h5>
-          </div>
-        </div>
-      </CardHeader>
-      <Link href={`/home/post/${post.post.id}`}>
-        <CardBody className="px-3 py-0 text-small  text-white">
-          <p>{post.post.content}</p>
-        </CardBody>
-        <CardFooter className="gap-3">
-          <button type="button">
-            <IconHeart className="w-4 h-4" color="#71767B" />
-          </button>
-          <div className="flex gap-1  text-zinc-500">
-            <ModalComment comments={post.comment} />
-          </div>
-        </CardFooter>
-      </Link>
+          </>
+          : <LoadingSkeleton />}
     </Card>
   );
 }
