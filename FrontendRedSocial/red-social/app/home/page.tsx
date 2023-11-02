1bcef5b67a19d6b5560c5010ff78fef82d4b53f2
osmait <saulburgos6@gmail.com>
2023-11-02 18:43:03 -0400

    change props of feed

--- FrontendRedSocial/red-social/app/home/page.tsx
+++ FrontendRedSocial/red-social/app/home/page.tsx
@@ -1,7 +1,6 @@
 import { cookies } from "next/headers";
 
 import { Feed } from "../components/feed";
-import { findProfilePost } from "../services/post.services";
 
 import { redirect } from "next/navigation";
 import { findAuthProfile } from "../services/userService";
@@ -12,14 +11,13 @@ export default async function Home() {
     redirect("/login");;
   }
   const user = await findAuthProfile(token.value)
-  const posts = await findProfilePost(user?.id, token.value);
 
   return (
     <div className=" flex  flex-col w-2/5 border-1 border-zinc-500 border-t-0">
       <div className="p-6 border-b-1 border-zinc-500">
         <h1 className="text-2xl font-bold ">Inicio</h1>
       </div>
-      <Feed posts={posts} />
+      <Feed id={user.id} token={token.value} />
     </div>
 
   );
