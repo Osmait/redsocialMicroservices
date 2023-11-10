d0335d6b1fb4104dc0321f120e3403f142d59ee1
osmait <saulburgos6@gmail.com>
2023-11-10 13:59:21 -0400

    userService: change userResponse LastName for lastName

--- userService/userService/src/main/java/com/sbsocial/userservice/infraestruture/dto/UserResponse.java
+++ userService/userService/src/main/java/com/sbsocial/userservice/infraestruture/dto/UserResponse.java
@@ -4,12 +4,12 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record UserResponse(
-        UUID id,
-        String name,
-        String LastName,
-        String email,
-        String address,
-        String phone,
-        LocalDateTime createdAt,
-        LocalDateTime updatedAt) {
+    UUID id,
+    String name,
+    String lastName,
+    String email,
+    String address,
+    String phone,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt) {
 }
