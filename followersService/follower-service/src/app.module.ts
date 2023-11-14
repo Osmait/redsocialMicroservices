1de1a584d3bc53a02e8b5c2a4b6acbde574bfc7c
osmait <saulburgos6@gmail.com>
2023-11-13 23:14:48 -0400

    add prometheus for follow-serice

--- followersService/follower-service/src/app.module.ts
+++ followersService/follower-service/src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FollowerModule } from './follower/follower.module';
-
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [FollowerModule, DatabaseModule],
+  imports: [FollowerModule, DatabaseModule, PrometheusModule.register()],
   controllers: [AppController],
   providers: [AppService],
 })
